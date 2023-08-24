76383a31e12493a876d019adc91b640147953691
Gonther1 <luisandresalvarezsilva@gmail.com>
2023-08-23 23:38:12 -0500

    primerDiv from main finish

--- bootstrap-5.3.1-examples/pricing/main.js
+++ bootstrap-5.3.1-examples/pricing/main.js
@@ -33,3 +33,64 @@ sectAddFistDiv()
 sectAddSecondDiv()
 /* Main */
 // First Div
+// Essential
+let mainFirstDivEs= async()=>{
+    let modificado= await fetch(`${path}.json`)
+    let res = await modificado.json();
+    let selecion = document.querySelector("#cards")
+    selecion.insertAdjacentHTML("beforeend",/* html */`        
+    <div class="col">
+          <div class="card mb-4 rounded-3 shadow-sm">
+            <div class="card-header py-3">
+              <h4 class="my-0 fw-normal">${res.main.primerDiv.essential.titulo}</h4>
+            </div>
+            <div class="card-body">
+              <h1 class="card-title pricing-card-title">${res.main.primerDiv.essential.precio}<small class="text-body-secondary fw-light">${res.main.primerDiv.essential.small}</small></h1>
+              <ul class="list-unstyled mt-3 mb-4">
+                <li>${res.main.primerDiv.essential.adittional.firstLi}</li>
+                <li>${res.main.primerDiv.essential.adittional.secondLi}</li>
+                <li>${res.main.primerDiv.essential.adittional.thirdLi}</li>
+                <li>${res.main.primerDiv.essential.adittional.fourthLi}</li>
+              </ul>
+              <button type="button" class="w-100 btn btn-lg btn-outline-danger">${res.main.primerDiv.essential.button}</button>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card mb-4 rounded-3 shadow-sm">
+            <div class="card-header py-3">
+              <h4 class="my-0 fw-normal">${res.main.primerDiv.standar.titulo}</h4>
+            </div>
+            <div class="card-body">
+              <h1 class="card-title pricing-card-title">${res.main.primerDiv.standar.precio}<small class="text-body-secondary fw-light">${res.main.primerDiv.standar.small}</small></h1>
+              <ul class="list-unstyled mt-3 mb-4">
+              <li>${res.main.primerDiv.standar.adittional.firstLi}</li>
+              <li>${res.main.primerDiv.standar.adittional.secondLi}</li>
+              <li>${res.main.primerDiv.standar.adittional.thirdLi}</li>
+              <li>${res.main.primerDiv.standar.adittional.fourthLi}</li>
+              </ul>
+              <button type="button" class="w-100 btn btn-lg btn-danger">${res.main.primerDiv.standar.button}</button>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card mb-4 rounded-3 shadow-sm border-danger">
+            <div class="card-header py-3 text-bg-danger border-danger">
+              <h4 class="my-0 fw-normal">${res.main.primerDiv.premium.titulo}</h4>
+            </div>
+            <div class="card-body">
+              <h1 class="card-title pricing-card-title">${res.main.primerDiv.premium.precio}<small class="text-body-secondary fw-light">${res.main.primerDiv.premium.small}</small></h1>
+              <ul class="list-unstyled mt-3 mb-4">
+                <li>${res.main.primerDiv.premium.adittional.firstLi}</li>
+                <li>${res.main.primerDiv.premium.adittional.secondLi}</li>
+                <li>${res.main.primerDiv.premium.adittional.thirdLi}</li>
+                <li>${res.main.primerDiv.premium.adittional.fourthLi}</li>
+              </ul>
+              <button type="button" class="w-100 btn btn-lg btn-danger">${res.main.primerDiv.premium.button}</button>
+            </div>
+          </div>
+        </div>
+        `
+    )
+}
+mainFirstDivEs()
